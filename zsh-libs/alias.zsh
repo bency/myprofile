c9918d2722f30d8fc8e33ed67cf242afa4a670f5
bency <bency@pixnet.tw>
2020-08-19 15:27:07 +0800

    add alias

---
+++ zsh-libs/alias.zsh
@@ -0,0 +1,11 @@
+alias t='tmux'
+alias ta='tmux attach -t'
+alias s="screen -e '\`~'"
+alias sr="screen -e '\`~' -r"
+alias sx="screen -e '\`~' -x"
+alias saqua='echo "This is aqua, may I help you ?"'
+alias composer='php -d allow_url_fopen=1 /usr/local/bin/composer'
+alias 男哥幫我拉='git pull'
+alias 男哥幫我推='git push'
+alias cdu='composer dumpautoload'
+alias sstaging='gcloud beta compute ssh --zone "asia-east1-b" "staging" --project "pixnet-platform-lab"'
